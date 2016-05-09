dbdcbe6355e0138422515bb90c48214a0e0df7da
Daniel Corregidor Coronado <daniel.corregidor@tajamar365.com>
2016-05-09 16:29:31 +0200

    Add project files.

---
+++ DemoSocial/Features/Feature1/Feature1.feature
@@ -0,0 +1,9 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="1b61d97e-21eb-43f2-98cf-80d51c9a3b5b" description="SharePoint Add-in Feature" featureId="1b61d97e-21eb-43f2-98cf-80d51c9a3b5b" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="DemoSocial Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="9d001aa6-3139-4d2e-a2d3-53d2a362feee" />
+    <projectItemReference itemId="9b228561-5cee-4ee9-b077-a21db469127b" />
+    <projectItemReference itemId="590fa58a-be1c-41bd-a283-856bd2027344" />
+    <projectItemReference itemId="ed70e580-8d33-4dd2-a5d6-dd1304d11ca0" />
+  </projectItems>
+</feature>
---
+++ DemoSocial/Scripts/App.js
@@ -0,0 +1,254 @@
+﻿'use strict';
+
+var posts = [];
+
+var resultados = [];
+
+function getQueryStringParameters(requestedParameter) {
+
+    var param = document.URL.split("?")[1].split("&");
+
+    var strParams = "";
+
+    for (var i = 0; i < param.LENGTH; i++) {
+        var actual = param[i].split("=");
+
+        if (actual[0] === requestedParameter) {
+            return actual[1];
+        }
+
+
+    }
+
+}
+
+var appWebUrl = decodeURIComponent(getQueryStringParameters("SPAppWebUrl"));
+
+
+var hostWebUrl = decodeURIComponent(getQueryStringParameters("SPHostUrl"));
+
+var clientContext = new  SP.ClientContext.get_current();
+
+var hostContext = SP.AppContextSite(clientContext, hostWebUrl);
+
+
+var formDigest = "";//necesito el formDigest para las operaciones de escritura
+
+var getFormDigest = function () {
+    $.ajax({
+        url: appWebUrl + "/_api/contextinfo",
+        type: "POST",
+        contentType: "application/json; odata=verbose",
+        headers: { 'accept': 'application/json; odata=verbose' },
+        success: function (data) {
+            formDigest = data.d.GetContextWebInformation.FormDigestValue;
+        },
+
+        error: function (xhr) {
+
+            alert(xhr.responseText);
+
+        },
+        async: false
+
+    });
+}
+();
+
+var getActorInfo = function (cuenta) {
+    var actor = "";
+
+    if (cuenta.indexOf("|") >= 0) {
+        cuenta = cuenta.split("|")[2];
+    }
+    $.ajax({
+        url: appWebUrl + "/_api/social.feed/actor(item='" + cuenta + "'",
+        //type: "GET",
+        //contentType: "application/json; odata=verbose",
+        headers: { 'accept': 'application/json; odata=verbose' },
+        success: function (data) {
+            actor = data.d.FollowableItemActor;
+
+        },
+
+        error: function (xhr) {
+
+            alert("Error " + xhr.responseText);
+
+        },
+        async: false
+    });
+
+    return actor;
+
+}
+
+var getSiteFeed = function () {
+
+    var feed;
+
+    $.ajax({
+        url: appWebUrl + "/_api/social.feed/actor(item=@v)/feed?@v='" + hostWebUrl + "/newsfeed.aspx'",
+        //type: "GET",
+        //contentType: "application/json; odata=verbose",
+        headers: { 'accept': 'application/json; odata=verbose' },
+        success: function (data) {
+            feed = getFeeds(data);
+        },
+
+        error: function (xhr) {
+            alert("Error " + xhr.responseText);
+        }
+    });
+
+    return feed;
+
+}
+
+
+function getFeeds(data) {
+    posts = data.d.SocialFeed.Threads.results.reverse();
+    var query =
+        "(ContentTypeId:0x01FD4FB0210AB50249908EAA47E6BD3CFE8B* " +
+            "OR ContentTypeId:0x01FD59A0DF25F1E14AB882D2C87D4874CF84* OR ContentTypeId:0x012002* " +
+            "OR ContentTypeId:0x0107* OR WebTemplate=COMMUNITY)";//owstaxIdMetadataAllTagsInfo:" + projectSiteCode; 
+
+    var keyWordQuery = new Microsoft.SharePoint.Client.Search.Query.KeywordQuery(clientContext);
+
+    keyWordQuery.set_queryText(query);
+
+    var lista = keyWordQuery.get_sortList();
+
+    lista.add("LastModifiedTime", Microsoft.SharePoint.Client.Search.Query.SortDirection.Ascending);
+
+    keyWordQuery.set_enableSorting(true);
+
+    var executor = new Microsoft.SharePoint.Client.Search.Query.SearchExecutor(clientContext);
+
+    var results = executor.executeQuery(keyWordQuery);
+
+    clientContext.executeQueryAsync(function () {
+        resultados = results.m_value.ResultTables[0].ResultRows;
+
+        updateDisplay();
+
+
+    },
+        function (e) {
+
+            alert("Error");
+
+        });
+
+}
+
+
+var updateDisplay = function () {
+    var postB;
+    var post;
+    var contenido = "<ul>";
+    var autor;
+
+
+    while (posts.length !== 0 || resultados.length !== 0) {
+        if (posts.length === 0) {
+            postB = resultados[resultados.length - 1];
+            autor = getActorInfo(postB.PostAuthor);
+            contenido += addToFeed(autor, postB.FullPostBody, postB.Created);
+            resultados.pop();
+
+        }
+        else if (resultados.length === 0) {
+            post = posts[posts.length - 1].RootPost;
+            autor = posts[posts.length - 1].Actors.results[post.AuthorIndex];
+
+            contenido += addToFeed(autor, post.FullPostBody, new Date(post.CreatedTime));
+
+            posts.pop();
+        }
+        else {
+            postB = resultados[resultados.length - 1];
+            post = posts[posts.length - 1].RootPost;
+            if (new Date(post.CreatedTime) > postB.Created) {
+                autor = posts[posts.length - 1].Actors.results[post.AuthorIndex];
+
+                contenido += addToFeed(autor, post.FullPostBody, new Date(post.CreatedTime));
+
+                posts.pop();
+            } else {
+
+                postB = resultados[resultados.length - 1];
+                autor = getActorInfo(postB.PostAuthor);
+                contenido += addToFeed(autor, postB.FullPostBody, postB.Created);
+                resultados.pop();
+
+            }
+
+        }
+    }
+
+    contenido += "</ul>";
+    $("#posts").html(contenido);
+
+}
+
+function addToFeed(autor, texto, fecha) {
+
+    var contenido = "<li>" + autor.name + "<br/>" + texto + "<br/>" + fecha + "</li>";
+
+    return contenido;
+
+}
+
+
+function sendPost() {
+
+    var contenido = $("#mensaje").val();
+
+
+    $.ajax({
+        url: appWebUrl + "/_api/social.feed/actor(item=@v)/feed/post?@v='" + hostWebUrl + "/newsfeed.aspx'",
+        type: "POST",
+        data: JSON.stringify({
+            "restCreationData": {
+                "__metadata": {
+                    "`type": "SP.Social.SocialRestPostCreationData"
+                },
+                "ID": null,
+                "creationData": {
+                    "__metadata": { "type": "SP.Social.SocialRestPostCreationData" },
+                    "ContentText": contenido,
+                    "UpdateStatusText": false
+                },
+            }
+        }),
+        headers: {
+            'accept': 'application/json; odata=verbose',
+            "content-type": "application/json; odata=verbose",
+            "X-RequestDigest": formDigest
+        },
+        success: getFeeds,
+
+        error: function () {
+            alert("Reventó");
+        }
+    });
+
+
+}
+
+
+
+$(document).ready(function() {
+    getSiteFeed();
+    $("#enviar").click(sendPost());
+
+
+
+})
+
+
+
+
+
+
